a269d861327777dae1b02701eb02305f97f9dd83
AngelMoraDev <angelmomaa@gmail.com>
2021-06-19 20:19:39 -0500

    replace build for create in article_spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Article, type: :model do
   context 'associations' do
     it 'ensures presence of title and text' do
-      author = build(:random_user)
+      author = create(:random_user)
       article = build(:random_article, user_id: author.id)
       expect(article.save).to eq(true)
     end
