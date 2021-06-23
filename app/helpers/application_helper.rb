6fd9233e47910197104d9dea1a1d5b185b76b964
AngelMoraDev <angelmomaa@gmail.com>
2021-06-23 15:09:40 -0500

    fix rubocop, add readme

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,8 @@ module ApplicationHelper
   def categories_menu(categories)
     categories_displayed = ''
     categories.each do |category|
-      categories_displayed << link_to(category.name.upcase, category_path(category), class: 'd-flex align-items-center simple-link')
+      categories_displayed << link_to(category.name.upcase, category_path(category),
+                                      class: 'd-flex align-items-center simple-link')
     end
     categories_displayed.html_safe
   end
