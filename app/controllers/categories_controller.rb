d8c83673d3a8c81bf99e75a25adadd1f90361491
AngelMoraDev <angelmomaa@gmail.com>
2021-06-25 17:32:39 -0500

    fix categories controller, modify categories show to display articles

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,7 +6,9 @@ class CategoriesController < ApplicationController
     @categories = Category.all
   end
 
-  def show; end
+  def show
+    @categories = @category.articles
+  end
 
   def new
     @category = Category.new
