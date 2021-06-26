38cb3f3ebbb434067b9524647c450016f0d1a901
AngelMoraDev <angelmomaa@gmail.com>
2021-06-26 12:04:00 -0500

    complete categories articles grid

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -7,16 +7,20 @@ module CategoriesHelper
     link_to(link_title, article_path(article), class: 'simple-link top-title m-2 p-2')
   end
 
-  def top_img(art, i)
+  def top_img(art, idx)
     img = ''
     #binding.pry
     img << image_tag(url_for(art.image.blob), class: 'half w-25vw bg-cover')
-    img.html_safe if i.odd?
+    img.html_safe if idx.odd?
   end
 
-  def bottom_img(art, i)
+  def bottom_img(art, idx)
     img = ''
     img << image_tag(url_for(art.image.blob), class: 'half w-25vw bg-cover')
-    img.html_safe if i.even?
+    img.html_safe if idx.even?
+  end
+
+  def read_more(art)
+    art.try { |a| a.text.truncate(150, separator: ' ', omission: '... To be continued.') }
   end
 end
