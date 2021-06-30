3111e94cfd78c4138089a86e9053e8af65b6e0ce
AngelMoraDev <angelmomaa@gmail.com>
2021-06-30 03:33:32 -0500

    troubleshoot aws3 setup to load images

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -2,6 +2,6 @@ Aws.config[:credentials] = Aws::Credentials.new(
   ENV['BUCKETEER_AWS_ACCESS_KEY_ID'],
   ENV['BUCKETEER_AWS_SECRET_ACCESS_KEY']
 )
-Aws.config[:region]      = 'us-east-1'
+Aws.config[:region]      = ENV['BUCKETEER_AWS_REGION']
 
 S3 = Aws::S3::Client.new
